a2689e9dc12cfddc0683437cabaf00396b8e0260
unknown <andreytsvl15@gmail.com>
2024-09-18 08:39:48 +0300

    sem_9_DZ_task66

---
+++ task66/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+// task 64 Задайте числа М и Н. Напишите программу, которая найдет сумму
+// натуральных элементов в промежутке от М до Н.
+Clear();
+Write("Введите число M ");
+int m = int.Parse(ReadLine());
+Write("Введите число N ");
+int n = int.Parse(ReadLine());
+if(m<n)
+{
+    WriteLine(SumRow(m, n));
+}
+else
+{
+    WriteLine(SumRow(n, m));
+}
+
+int SumRow(int m, int n)
+{
+    if (m == n) return m;
+    return (n + SumRow(m, n - 1));
+}
